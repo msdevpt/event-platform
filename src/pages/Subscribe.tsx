7a3b4289392e4d95a574696760ea2b55133d682b
M.Silva <msilva.kb@outlook.com>
2022-06-26 18:33:09 +0100

    feat: add favicon and fix type error

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -72,7 +72,7 @@ export function Subscribe(){
 
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src="/src/assets/code-mockup.png" className="mt-10" alt="code mockup" />
     </div>
   )
 }
